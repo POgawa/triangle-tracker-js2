1eb30983e7668015fda9741fddf2d748121bb244
Paul Ogawa & Jim Patt <student@epicodus.com>
2015-05-26 11:26:16 -0700

    Add passing triangle validation

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('isTriangle', function() {
   it("returns false if a triangle is invalid", function() {
     expect(isTriangle(2,2,8)).to.equal(false);
   });
+  it("returns true if a triangle is valid", function() {
+    expect(isTriangle(2,2,2)).to.equal(true);
+  });
+
 });
