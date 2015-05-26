8587fbbb2945e7b6d18219134e82ac9d43053f3f
Paul Ogawa & Jim Patt <student@epicodus.com>
2015-05-26 12:08:08 -0700

    return scalene for a traingle with unequal sides and isoscles for a triangle with two of the same sides

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,11 @@ var triangleTracker = function(side1, side2, side3) {
 
     if (side1 === side2 && side2 === side3) {
       return("equilateral")
-
+    }
+    else if (side1 !== side2 && side2 !== side3 && side1 !== side3 ) {
+      return('scalene')
+    } else {
+      return 'isosceles'
     }
   } else {
     return "not a valid triangle"
--- spec/specs.js
+++ spec/specs.js
@@ -9,14 +9,20 @@ describe('isTriangle', function() {
 });
 
 describe('triangleTracker', function() {
-  it("returns the type of triangle", function() {
+  it("returns equilateral for an equilateral triangle", function() {
     expect(triangleTracker(2,2,2)).to.equal("equilateral");
   });
 
-
-
   it("returns that a triangle is not a valid triangle", function() {
     expect(triangleTracker(1,1,6)).to.equal("not a valid triangle");
+  });
+
+  it("returns scalene if the triangle is scalene", function() {
+    expect(triangleTracker(1,2,3)).to.equal("scalene");
+  });
+
+  it("returns isoscles if the triangle is isoceles", function() {
+    expect(triangleTracker(3,3,6)).to.equal("isosceles");
   });
 
 });
