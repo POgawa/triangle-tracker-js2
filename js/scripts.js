668eea9c6512668a562473542079e87020e4d770
Paul Ogawa & Jim Patt <student@epicodus.com>
2015-05-26 11:53:56 -0700

    return equillateral or not a valid triangle accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ var isTriangle = function(side1, side2, side3) {
   }
 
 };
+
+var triangleTracker = function(side1, side2, side3) {
+  if (isTriangle(side1, side2, side3)) {
+
+    if (side1 === side2 && side2 === side3) {
+      return("equilateral")
+
+    }
+  } else {
+    return "not a valid triangle"
+  }
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,8 +2,21 @@ describe('isTriangle', function() {
   it("returns false if a triangle is invalid", function() {
     expect(isTriangle(2,2,8)).to.equal(false);
   });
+
   it("returns true if a triangle is valid", function() {
     expect(isTriangle(2,2,2)).to.equal(true);
+  });
+});
+
+describe('triangleTracker', function() {
+  it("returns the type of triangle", function() {
+    expect(triangleTracker(2,2,2)).to.equal("equilateral");
+  });
+
+
+
+  it("returns that a triangle is not a valid triangle", function() {
+    expect(triangleTracker(1,1,6)).to.equal("not a valid triangle");
   });
 
 });
